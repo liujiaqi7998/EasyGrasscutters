ba7dad2a2ee99ede6f3259736a6ea9cd9331d3bd
liujiaqi7998 <1792498113@qq.com>
2022-08-29 20:22:54 +0800

    完成对剧情添加完成功能

--- src/main/java/top/cyqi/EasyGrasscutters/ServerUtils/Main.java
+++ src/main/java/top/cyqi/EasyGrasscutters/ServerUtils/Main.java
@@ -3,6 +3,7 @@ package top.cyqi.EasyGrasscutters.ServerUtils;
 import emu.grasscutter.Grasscutter;
 import emu.grasscutter.command.CommandMap;
 import emu.grasscutter.game.player.Player;
+import emu.grasscutter.game.quest.GameQuest;
 import io.javalin.websocket.WsMessageContext;
 import org.json.JSONObject;
 
@@ -28,7 +29,7 @@ public class Main {
                         JSONObject temp = new JSONObject();
                         temp.put("type", "error");
                         temp.put("msg_id", object.getString("msg_id"));
-                        temp.put("data", "用户不在线");
+                        temp.put("data", "玩家不在线");
                         wsMessageContext.send(temp.toString());
                         return;
                     }
@@ -49,7 +50,100 @@ public class Main {
                 temp.put("msg_id", object.getString("msg_id"));
                 temp.put("data", number);
                 wsMessageContext.send(temp.toString());
-                return;
+            }
+            case "GetPlayerLocation" -> {
+                String player_uid = object.getString("player_uid");
+                Player tmp = getGameServer().getPlayers().get(Integer.valueOf(player_uid));
+                if (tmp == null) {
+                    JSONObject temp = new JSONObject();
+                    temp.put("type", "error");
+                    temp.put("msg_id", object.getString("msg_id"));
+                    temp.put("data", "玩家不在线");
+                    wsMessageContext.send(temp.toString());
+                    return;
+                }
+
+                JSONObject temp = new JSONObject();
+                temp.put("type", "GetPlayerLocation");
+                temp.put("msg_id", object.getString("msg_id"));
+                temp.put("X", tmp.getPlayerLocationInfo().getPos().getX());
+                temp.put("Y", tmp.getPlayerLocationInfo().getPos().getY());
+                temp.put("Z", tmp.getPlayerLocationInfo().getPos().getZ());
+                temp.put("scene", tmp.getSceneId());
+                wsMessageContext.send(temp.toString());
+            }
+
+            case "GetPlayerBirthday" -> {
+                String player_uid = object.getString("player_uid");
+                Player player = getGameServer().getPlayerByUid(Integer.parseInt(player_uid), true);
+                if (player == null) {
+                    JSONObject temp = new JSONObject();
+                    temp.put("type", "error");
+                    temp.put("msg_id", object.getString("msg_id"));
+                    temp.put("data", "玩家不存在");
+                    wsMessageContext.send(temp.toString());
+                    return;
+                }
+                JSONObject temp = new JSONObject();
+                temp.put("type", "GetPlayerBirthday");
+                temp.put("msg_id", object.getString("msg_id"));
+                temp.put("Month", player.getBirthday().getMonth());
+                temp.put("Day", player.getBirthday().getDay());
+                wsMessageContext.send(temp.toString());
+            }
+
+            case "QuestAction" -> {
+                String player_uid = object.getString("player_uid");
+                Player player = getGameServer().getPlayerByUid(Integer.parseInt(player_uid), false);
+                if (player == null) {
+                    JSONObject temp = new JSONObject();
+                    temp.put("type", "error");
+                    temp.put("msg_id", object.getString("msg_id"));
+                    temp.put("data", "玩家不在线");
+                    wsMessageContext.send(temp.toString());
+                    return;
+                }
+                GameQuest que = player.getQuestManager().addQuest(object.getInt("Quest_id"));
+                JSONObject temp = new JSONObject();
+                temp.put("type", "QuestAction");
+                temp.put("msg_id", object.getString("msg_id"));
+                if (que != null) {
+                    temp.put("data", true);
+                } else {
+                    temp.put("data", false);
+                }
+                wsMessageContext.send(temp.toString());
+            }
+
+            case "QuestFinish" -> {
+                String player_uid = object.getString("player_uid");
+                Grasscutter.getLogger().info("1");
+                Player player = getGameServer().getPlayerByUid(Integer.parseInt(player_uid), false);
+                if (player == null) {
+                    JSONObject temp = new JSONObject();
+                    temp.put("type", "error");
+                    temp.put("msg_id", object.getString("msg_id"));
+                    temp.put("data", "玩家不在线");
+                    wsMessageContext.send(temp.toString());
+                    return;
+                }
+                Grasscutter.getLogger().info("2");
+                GameQuest quest = player.getQuestManager().addQuest(object.getInt("Quest_id"));
+                Grasscutter.getLogger().info("3");
+                if (quest == null) {
+                    JSONObject temp = new JSONObject();
+                    temp.put("type", "error");
+                    temp.put("msg_id", object.getString("msg_id"));
+                    temp.put("data", "玩家不存在该剧情");
+                    wsMessageContext.send(temp.toString());
+                    return;
+                }
+                quest.finish();
+                JSONObject temp = new JSONObject();
+                temp.put("type", "QuestFinish");
+                temp.put("msg_id", object.getString("msg_id"));
+                temp.put("data", true);
+                wsMessageContext.send(temp.toString());
             }
         }
     }
